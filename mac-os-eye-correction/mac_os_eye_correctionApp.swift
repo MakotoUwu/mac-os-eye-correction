92d1743f68b2de7be8b2d6ef2a20ff1c2f91e4e6
MakotoUwu <tsepuh.mamoru@gmail.com>
2025-05-04 13:48:48 +0200

    Initial Commit

---
+++ mac-os-eye-correction/mac_os_eye_correctionApp.swift
@@ -0,0 +1,17 @@
+//
+//  mac_os_eye_correctionApp.swift
+//  mac-os-eye-correction
+//
+//  Created by Oleksandr Tsepukh on 04/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct mac_os_eye_correctionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mac-os-eye-correctionTests/mac_os_eye_correctionTests.swift
@@ -0,0 +1,17 @@
+//
+//  mac_os_eye_correctionTests.swift
+//  mac-os-eye-correctionTests
+//
+//  Created by Oleksandr Tsepukh on 04/05/2025.
+//
+
+import Testing
+@testable import mac_os_eye_correction
+
+struct mac_os_eye_correctionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
